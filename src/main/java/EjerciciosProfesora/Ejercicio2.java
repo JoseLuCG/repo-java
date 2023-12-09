cd652e671ed9baa92ef037c3e27ac024cd151a65
Jose Luis <condegomesjoseluis@gmail.com>
2023-12-09 22:05:57 +0100

    Add a new function.

--- src/main/java/EjerciciosProfesora/Ejercicio2.java
+++ src/main/java/EjerciciosProfesora/Ejercicio2.java
@@ -1,2 +1,21 @@
-package EjerciciosProfesora;public class Ejercicio2 {
+/* 2.
+    Hacer un programa que averigüe si una matriz cuadrada de orden 4 es
+simétrica. Al declarar la matriz asignarle valores que representen una
+matriz simétrica. Probar el programa con la matriz inicial, y después
+cambiar uno de los valores para que no sea simétrica. Una matriz es
+simétrica cuando a[f][c]=a[c][f] para cualquier elemento de la matriz.
+ */
+package EjerciciosProfesora;
+import static EjerciciosProfesora.Ejercicio1.showMatrix;
+
+public class Ejercicio2 {
+    public static void symmetricMatrixCreator (int[][] matrix) {
+
+    }
+    public static void main(String[] args) {
+        int[][] symmetricalMatrix = new int[4][4]; //Declares the symetrical matrix.
+
+        //Print the table.
+        showMatrix(symmetricalMatrix);
+    }
 }
