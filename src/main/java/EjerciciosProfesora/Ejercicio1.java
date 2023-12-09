98a3587326f9643493f76901c17688f5fcff6a95
Jose Luis <condegomesjoseluis@gmail.com>
2023-12-09 21:03:36 +0100

    Add exercise 1 of pdf.

---
+++ src/main/java/EjerciciosProfesora/Ejercicio1.java
@@ -0,0 +1,10 @@
+/* 1.
+    Hacer un programa que genere y escriba una matriz unitaria de orden 10.
+Una matriz unitaria de orden 10 es una matriz de 10x10 en la que todos los
+elementos valen 0, excepto los de la diagonal principal que valen 1. La diagonal
+principal es la que va de la esquina superior izquierda a la inferior derecha. */
+package EjerciciosProfesora;
+
+public class Ejercicio1 {
+
+}
