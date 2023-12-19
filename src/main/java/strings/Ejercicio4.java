411ea543328efedc01c56fc06198b8fa215632a0
unknown <DAW106@acarballeira.local>
2023-12-19 09:53:27 +0100

    Exercise 4, Complete.

--- src/main/java/strings/Ejercicio4.java
+++ src/main/java/strings/Ejercicio4.java
@@ -3,14 +3,17 @@ Escriba un programa que lea una frase y a continuación visualice cada palabra
 de la frase en columnas, seguida del número de letras que tiene cada palabra. */
 package strings;
 
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 public class Ejercicio4 {
+    public static PrintStream printOut = new PrintStream(System.out,true, StandardCharsets.UTF_8);
     public static void counterWords (String textUSer) {
         String[] textWords = textUSer.split("[,.;: ]+");
 
         for (int word = 0; word < textWords.length; word++) {
-            System.out.println(textWords[word]+" ==> Tiene: "+textWords[word].length()+" letras");
+            printOut.println(textWords[word]+" ==> Tiene: "+textWords[word].length()+" letras");
         }
     }
     public static void main(String[] args) {
